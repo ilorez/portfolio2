eca604ca780c0165c1795132d2b43897d63f8f46
ilorez <zobirnajdaoui@gmail.com>
2024-11-30 21:48:29 +0100

    chore: Update next.config.js to set output as 'standalone'

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
 const path = require('path')
- 
 module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
-      },
-    ],
-  },
-}
+  output: 'standalone',
+};

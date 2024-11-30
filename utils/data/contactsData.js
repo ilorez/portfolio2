19c66f754bd07ae7697d70e62a92e07e820ddd21
ilorez <zobirnajdaoui@gmail.com>
2024-11-30 21:03:23 +0100

    some update

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'zobirnajdaoui@gmail.com',
+    phone: '+212615331474',
+    address: 'Douar Sidi Moussa Tasseloutant, Marrakech, Morocco',
+    github: 'https://github.com/ilorez',
+    facebook: 'https://www.facebook.com/ilorz/',
+    linkedIn: 'https://www.linkedin.com/in/ilorez/',
+    twitter: 'https://twitter.com/ilorez',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/ilorez',
+    devUsername: "ilorez",
 }
--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,26 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
+    title: "1337",
+    duration: "2022 - 2024",
     institution: "National University of Bangladesh",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Ista ntic SYBA",
+    duration: "2022 - 2024",
+    institution: "National University of Bangladesh",
   },
   {
     id: 3,
+    title: "Higher Secondary Certificate",
+    duration: "2019 - 2021",
+    institution: "Noakhali Islamia Kamil Madrasah",
+  },
+  {
+    id: 4,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
+    duration: "2009 - 2019",
     institution: "Baitus Saif Islamia Madrasah",
   }
 ]

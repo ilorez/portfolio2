4403deb0d27f28659986ff3db789ccb3693b58ce
ilorez <zobirnajdaoui@gmail.com>
2025-01-05 08:23:29 +0100

    chore: Remove unused imports and commented code in app/page.js

--- app/page.js
+++ app/page.js
@@ -1,31 +1,43 @@
 'use client'
 
-import { personalData } from "@/utils/data/personal-data";
+// import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
+// import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+// import { useEffect } from "react";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+// async function getData() {
+//   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+//   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
 
-  return filtered;
-};
+//   return filtered;
+// };
 
-export default async function Home() {
-  const blogs = await getData();
+export default function Home() {
+  // let blogs = [];
+  // useEffect(() => {
+  // //const blogs = await getData();
+
+  // getData().then((data) => {
+  //   blogs = data;
+  // }).catch((error) => {
+  //   console.error(error);
+  // });
+
+  // }, []);
+  
 
   return (
     <>
@@ -35,7 +47,7 @@ export default async function Home() {
       <Skills />
       <Projects />
       <Education />
-      <Blog blogs={blogs} />
+      {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </>
   )
